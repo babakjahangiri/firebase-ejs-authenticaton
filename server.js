1950b576e57208107a1ee25d9f1819400dedf40d
Babak <babakjahangiri@gmail.com>
2020-12-04 22:25:37 +0000

    add csrf

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const cookieParser = require("cookie-parser");
-const csurf = require("cookie-parser");
-const bodyParser = require("body-parser");
-const express = require("express");
+const cookieParser = require('cookie-parser'); //for write & read cookies
+const csrf = require('csurf'); //to prevent us from CSRF(cross site request frogery) attacks
+const bodyParser = require('body-parser');
+const express = require('express');
 
-const csrfMiddleware = csurf({cookie:true});
+//check to find the require cookies in each request
+const csrfMiddleware = csrf({cookie:true});
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+//parse post request from the frontend
+app.use(bodyParser.json());
+app.use(cookieParser());
+//sets & checks csrf related cookies 
+app.use(csrfMiddleware);
 
 app.engine("html", require('ejs').renderFile);
 app.use(express.static('static'));
 
-app.all('*' , (req,res,next) => {
-//res.cookie("XSRF-TOKEN", req.csrfToken());
+app.all('*' , (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
 next();
 });
 
