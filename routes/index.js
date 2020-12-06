8059aecf8a6304e9ba690d96a9dc4aac083dc8c6
Babak <babakjahangiri@gmail.com>
2020-12-06 02:52:40 +0000

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {firebase_auth} = require("../firebaseService")
+
 module.exports = () => {
 
   router.get('/', async (req,res) => {
@@ -18,19 +20,53 @@ module.exports = () => {
   })
 
   router.get('/profile', async (req,res) => {
-    return res.render('layout', {
-      pageTitle : 'User Profile',
-      template : 'profile'
-    });
-  })
+    const sessionCookie = req.cookies.session || "";
+    
+    firebase_auth().verifySessionCookie(sessionCookie, true /**checkRevoked */)
+     .then(()=> {
+      return res.render('layout', {
+        pageTitle : 'User Profile',
+        template : 'profile'
+      });
+     })
+
+      .catch((error) => {
+        res.redirect("/login");
+     });
+  });
 
   router.get('/signup', async (req,res) => {
     return res.render('layout', {
       pageTitle : 'signup',
       template : 'signup'
     });
-  })
+  });
 
+  router.post("/sessionLogin", (req, res) => {
+    const idToken = req.body.idToken.toString();
+  
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  
+    firebase_auth()
+      .createSessionCookie(idToken, { expiresIn })
+      .then(
+        (sessionCookie) => {
+          const options = { maxAge: expiresIn, httpOnly: true };
+          res.cookie("session", sessionCookie, options);
+          res.end(JSON.stringify({ status: "success" }));
+        },
+        (error) => {
+          res.status(401).send("UNAUTHORIZED REQUEST!");
+        }
+      );
+  });
+
+  router.get("/sessionLogout", (req, res) => {
+    res.clearCookie("session");
+    res.redirect("/login");
+  });
+
+  
   return router;
 
 };
